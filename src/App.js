fc9c005b5f694676cd46623b35cde6ae53b8a1d8
dejanle <dejnik978@gmail.com>
2017-06-15 22:51:28 +0200

    add loop in React app

--- src/App.js
+++ src/App.js
@@ -2,16 +2,26 @@ import React, { Component } from 'react';
 import Navbar from './components/Navbar.js';
 import Jumbotron from './components/Jumbotron';
 import Content from './components/Content.js';
+import nature from './nature.json';
 import './App.css';
 
 class App extends Component {
- 
+
+ renderContents() {
+ 	let resultsArray = [];
+ 	nature.nature_news.map((item, i) => {
+ 		resultsArray.push(<Content item={item} key={i} />)
+ 	});
+ 	return resultsArray;
+ }
+
   render() {
     return ( 
      <div>
-       <Navbar />,
-       <Jumbotron />, 
-       <Content />
+       <Navbar /> 
+       <Jumbotron />  
+       {this.renderContents()}
+       
      </div> 
     ) 
   }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,9 +18,9 @@ class Navbar extends Component{
 			        </div>
 			        <div id="navbar" className="collapse navbar-collapse">
 			          <ul className="nav navbar-nav">
-			            <li><a href="">Home</a></li>
-			            <li><a href="">About</a></li>
-			            <li><a href="">Contact</a></li>
+			            <li><a href="/">Home</a></li>
+			            <li><a href="/about">About</a></li>
+			            <li><a href="/contact">Contact</a></li>
 			          </ul>
 			        </div> 
 			      </div>
